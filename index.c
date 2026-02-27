343860005a869af6d7c2a22888fa0454de04817a
adarsh kushwaha <ak6862219@gmail.com>
2026-02-27 17:48:09 +0000

    update

--- index.c
+++ index.c
@@ -106,3 +106,17 @@
     
 //     return 0;
 // }
+
+//check even and odd
+// #include<stdio.h>
+// int main(){
+//     int a;
+//     printf("Enter a: ");
+//     scanf("%d",&a);
+//     if(a%2==0){
+//         printf("Even");
+//     }else{
+//         printf("Odd");
+//     }
+//     return 0;
+// }
