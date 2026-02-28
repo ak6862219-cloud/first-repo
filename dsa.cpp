3f9d963069cf0a16dd159d5efe889d97bdbab0dd
adarsh kushwaha <ak6862219@gmail.com>
2026-02-28 13:03:50 +0000

    add

--- dsa.cpp
+++ dsa.cpp
@@ -70,3 +70,21 @@
 //     cout<< "Factorial of "<< n <<" is "<<factorial(n)<<endl;
 //     return 0;
 // }
+
+//4. Trailing Zeros in factorial
+// #include<iostream>
+// using namespace std;
+// int CountTrailingZeros(int n){
+//     int res =0;
+//     for(int i =5;i<n;i*=5){
+//         res +=(n/i);
+//     }
+//     return res;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+//     cout<<"Trailing Zeros: "<<CountTrailingZeros(n)<<endl;
+//     return 0;
+// }
