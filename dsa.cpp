e61a0456fffdd7c37c7725e996da80d93f04deb2
adarsh kushwaha <ak6862219@gmail.com>
2026-02-28 13:05:06 +0000

    add

--- dsa.cpp
+++ dsa.cpp
@@ -88,3 +88,27 @@
 //     cout<<"Trailing Zeros: "<<CountTrailingZeros(n)<<endl;
 //     return 0;
 // }
+
+//5. Greatest common divisor(GCD)
+// #include<iostream>
+// using namespace std;
+
+// int gcd(int a, int b){
+//     int res =min(a,b);
+//     while(res>0){
+//         if(a%res==0 && b%res ==0){
+//             break;
+//         }
+//         res--;
+//     }
+//     return res;
+// }
+// int main(){
+//     int a,b;
+//     cout<<"Enter a: ";
+//     cin>>a;
+//     cout<<"Enter b: ";
+//     cin>>b;
+//     cout<<"gcd is: "<<gcd(a,b)<<endl;
+//     return 0;
+// }
